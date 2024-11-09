f704560cbd4d48cad3ed86625a43d34dc526a884
Chiper Andrei <138249161+chiperandrei42@users.noreply.github.com>
2024-11-09 21:17:27 +0200

    first task

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const stopButton = document.querySelector("[data-stop]");
+const startButton = document.querySelector("[data-start]");
 
+startButton.addEventListener('click', changeColor);
+stopButton.addEventListener('click', stopChangingColor);
+
+let changeColorInterval;
+
+function changeColor() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    bgColorFunction();
+    changeColorInterval = setInterval(bgColorFunction, 1000);
+}
+
+function stopChangingColor() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(changeColorInterval);
+}
+
+function bgColorFunction() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
